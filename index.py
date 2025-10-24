24c7ef524ec2d6a130c99910bcdc1f4d5bc8a15d
Ajasajafar <bolarinwaayorinde97@gmail.com>
2025-10-24 15:24:05 +0100

    Learning numpy
    Why is NumPy better than lists

--- index.py
+++ index.py
@@ -1,3 +1,8 @@
-name = "Ajasa"
-if name.upper == "AJASA":
-    print('You are my namesake fam!')
+# name = "Ajasa"
+# if name.upper == "AJASA":
+#     print('You are my namesake fam!')
+
+import numpy 
+a = numpy.array([1,3,5])
+b = numpy.array([1,2,3])
+print(a*b)
