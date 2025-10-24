a75aab4b33acd5998445edc0ca291b412588861a
Ajasajafar <bolarinwaayorinde97@gmail.com>
2025-10-24 18:02:14 +0100

    NumPy Syntax

--- index.py
+++ index.py
@@ -8,11 +8,29 @@ import numpy as np
 # print(a*b)
 
 # The basics
-a = np.array([1,3,2])
-print(a)
+# a = np.array([1,3,2], dtype ='int32')
+# print(a)
 
-b = np.array([[9.0,8.0,7.0],[2.0,3.0,1.0]])
-print(b)
-# Get dimension
-print(a.ndim)
-print(b.ndim)
+# b = np.array([[9.0,8.0,7.0],[2.0,3.0,1.0]], dtype ='int16')
+# print(b)
+# # Get dimension
+# print(a.ndim)
+# print(b.ndim)
+
+# # Get shape and getting type
+# print(a.dtype)
+# print(b.dtype)
+# print(a.shape)
+# print(b.shape)
+
+# # Get size
+# print(a.itemsize)
+# print(b.itemsize)
+
+# # Get total size
+# print(a.nbytes)
+
+
+# Accessing/Changing specific elements, rows, columns, etc.
+a = np.array([[1,2,3,4,5,6,7],[8,9,10,11,12,13,14]])
+print(a.shape)
