79cac7fc3f15e45b4958a1db42372a095fa298fa
Ajasajafar <bolarinwaayorinde97@gmail.com>
2025-10-24 16:46:56 +0100

    Getting Dimension

--- index.py
+++ index.py
@@ -2,7 +2,17 @@
 # if name.upper == "AJASA":
 #     print('You are my namesake fam!')
 
-import numpy 
-a = numpy.array([1,3,5])
-b = numpy.array([1,2,3])
-print(a*b)
+import numpy as np
+# a = [1,3,5]
+# b = [1,2,3]
+# print(a*b)
+
+# The basics
+a = np.array([1,3,2])
+print(a)
+
+b = np.array([[9.0,8.0,7.0],[2.0,3.0,1.0]])
+print(b)
+# Get dimension
+print(a.ndim)
+print(b.ndim)
